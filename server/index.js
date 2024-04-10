baec5d7c70399f3f060bca91a9172a5003b67477
Emperor <jainchist@gmail.com>
2024-04-10 15:12:12 +0530

    init2

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"http://study-notion-client-five.vercel.app",
 		credentials:true,
 	})
 )
